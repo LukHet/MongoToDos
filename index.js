8c45d435fbf7e4a87061b491120f5b34a2b47a46
LukHet <lukaszhetman1@gmail.com>
2024-02-08 00:16:38 +0100

    Added displaying current status of base, fixed minor bugs.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb");
-const mongo = require("mongodb");
 
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url, { family: 4 });
@@ -16,11 +15,41 @@ const addNewTodo = async (todosCollection, title) => {
     .catch((err) => console.log("Błąd dodawania", err));
 };
 
+const displayingTodos = (todosCollection) => {
+  const todosToDo = todosCollection.filter((todo) => !todo.done);
+  const todosDone = todosCollection.filter((todo) => todo.done);
+  console.log(`# Lista zadań do zrobienia - ${todosToDo.length} zadań`);
+  for (const todo of todosToDo) {
+    console.log(
+      `- ${todo.title} - ${todo.done ? "Zakończony" : "Do zrobienia"}`
+    );
+  }
+  console.log(`# Lista zadań zrobionych - ${todosDone.length} zadań`);
+  for (const todo of todosDone) {
+    console.log(
+      `- ${todo.title} - ${todo.done ? "Zakończony" : "Do zrobienia"}`
+    );
+  }
+};
+
+const showAllTodos = async (todosCollection) => {
+  const toDos = await todosCollection
+    .find()
+    .toArray()
+    .then((data) => console.log(displayingTodos(data)))
+    .catch((err) => console.log("Błąd wyświetlania bazy", err));
+};
+
 const doTheToDo = async (todosCollection) => {
   const [command, ...args] = process.argv.splice(2);
   switch (command) {
     case "add":
       await addNewTodo(todosCollection, args[0]);
+      break;
+  }
+  switch (command) {
+    case "list":
+      await showAllTodos(todosCollection, args[0]);
       break;
   }
 };
