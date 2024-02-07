b30f5966f3cb192828a822838198c2f79d2b2703
LukHet <lukaszhetman1@gmail.com>
2024-02-07 23:22:21 +0100

    Implemented adding tasks

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const client = new MongoClient(url, { family: 4 });
 
 const dbName = "test";
 
-async function main() {
-  await client.connect().then(console.log("Connected successfully to server"));
+const addNewTodo = async (todosCollection, title) => {
+  await todosCollection
+    .insertOne({
+      title,
+      done: false,
+    })
+    .then((msg) => console.log("Udało się dodać do bazy", msg))
+    .catch((err) => console.log("Błąd dodawania", err));
+};
 
+const doTheToDo = async (todosCollection) => {
+  const [command, ...args] = process.argv.splice(2);
+  switch (command) {
+    case "add":
+      await addNewTodo(todosCollection, args[0]);
+      break;
+  }
+};
+
+const handleRequest = () => {
+  console.log("Connected successfully to server");
   const db = client.db(dbName);
   const todosCollection = db.collection("todos");
+  doTheToDo(todosCollection);
+};
 
-  client.close();
-}
-
-main();
+client.connect().then(handleRequest());
