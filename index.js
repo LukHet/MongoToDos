2fdc4b1054eacdae797f692ea06e2ab4f8bdf505
LukHet <lukaszhetman1@gmail.com>
2024-02-11 23:52:58 +0100

    Added option of updating tasks.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
+const mongo = require("mongodb");
 
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url, { family: 4 });
@@ -15,19 +16,41 @@ const addNewTodo = async (todosCollection, title) => {
     .catch((err) => console.log("Błąd dodawania", err));
 };
 
+const markTaskAsDone = (todosCollection, id) => {
+  todosCollection
+    .updateOne(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: {
+          done: true,
+        },
+      }
+    )
+    .then((msg) =>
+      console.log("Udało zmienić status zadania na zrobiony ", msg)
+    )
+    .catch((err) => console.log("Błąd zmieniania statusu ", err));
+};
+
 const displayingTodos = (todosCollection) => {
   const todosToDo = todosCollection.filter((todo) => !todo.done);
   const todosDone = todosCollection.filter((todo) => todo.done);
   console.log(`# Lista zadań do zrobienia - ${todosToDo.length} zadań`);
   for (const todo of todosToDo) {
     console.log(
-      `- ${todo.title} - ${todo.done ? "Zakończony" : "Do zrobienia"}`
+      `- id: ${todo._id} nazwa: ${todo.title} - ${
+        todo.done ? "Zakończony" : "Do zrobienia"
+      }`
     );
   }
   console.log(`# Lista zadań zrobionych - ${todosDone.length} zadań`);
   for (const todo of todosDone) {
     console.log(
-      `- ${todo.title} - ${todo.done ? "Zakończony" : "Do zrobienia"}`
+      `- id: ${todo._id} nazwa: ${todo.title} - ${
+        todo.done ? "Zakończony" : "Do zrobienia"
+      }`
     );
   }
 };
@@ -46,10 +69,11 @@ const doTheToDo = async (todosCollection) => {
     case "add":
       await addNewTodo(todosCollection, args[0]);
       break;
-  }
-  switch (command) {
     case "list":
-      await showAllTodos(todosCollection, args[0]);
+      await showAllTodos(todosCollection);
+      break;
+    case "done":
+      await markTaskAsDone(todosCollection, args[0]);
       break;
   }
 };
